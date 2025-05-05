774d46ca7793b23bee2f912156053f86d9fd20d3
Valentin <latronixo@gmail.com>
2025-05-05 19:00:17 +0300

    Initial Commit

---
+++ 27. Project 5 (Game using closures)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  27. Project 5 (Game using closures)
+//
+//  Created by Валентин Картошкин on 05.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
